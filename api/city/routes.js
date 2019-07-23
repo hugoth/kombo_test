e06329dc428726f4b71814754185f9b92e944bbe
Hugo Tessier <htessierhuort@gmail.com>
2019-07-23 12:39:57 +0200

    clean logs in controllers

--- api/city/routes.js
+++ api/city/routes.js
@@ -4,5 +4,5 @@ const controller = require("./controllers");
 
 router.post("/city/create", controller.createCity);
 router.get("/cities", controller.getCities);
-router.post("/city/search/", controller.searchCity);
+router.post("/city/search", controller.searchCity);
 module.exports = router;
